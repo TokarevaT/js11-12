c5eec1c64de6b1704a69369879d3e017781bac47
Tokareva Tanya <tokareva-tatyanka@mail.ru>
2016-05-11 01:51:35 +0300

    add carousel plugin

---
+++ js11-12/js/JavaScript1.js
@@ -0,0 +1,42 @@
+﻿
+$(document).ready(function () {
+    var imgs = [
+        { src: '../img/108040.jpg', text: 'Описание 1' },
+        { src: '../img/1252077103_240x320_wallpapers-12.jpg', text: 'Описание 2' },
+    { src: '../img/133.jpg', text: 'Описание 3' },
+    { src: '../img/dragon_3.gif', text: 'Описание 4' },
+    { src: '../img/tiger.jpg', text: 'Описание 5' },
+    { src: '../img/images.jpg', text: 'Описание 6' },
+    { src: '../img/tigr_5.jpg', text: 'Описание 7' },
+    { src: '../img/192515785.jpg', text: 'Описание 8' },
+    { src: '../img/426417157.jpg', text: 'Описание 9' },
+    { src: '../img/images (1).jpg', text: 'Описание 10' },
+    { src: '../img/239432549.jpg', text: 'Описание 11' },
+    { src: '../img/flavers_2.gif', text: 'Описание 12' }
+    ];
+
+    $('.carousel').myFirstPlugin(imgs);
+    //var leftUIEl = $('.carousel-arrow-left');
+    //var rightUIEl = $('.carousel-arrow-right');
+    //var elementsList = $('.carousel-list');
+
+    //var pixelsOffset = 240;
+    //var currentLeftValue = 0;
+    //var elementsCount = elementsList.find('li').length;
+    //var minimumOffset = -((elementsCount - 5) * pixelsOffset);
+    //var maximumOffset = 0;
+
+    //leftUIEl.click(function () {
+    //    if (currentLeftValue != maximumOffset) {
+    //        currentLeftValue += 240;
+    //        elementsList.animate({ left: currentLeftValue + "px" }, 500);
+    //    }
+    //});
+
+    //rightUIEl.click(function () {
+    //    if (currentLeftValue != minimumOffset) {
+    //        currentLeftValue -= 240;
+    //        elementsList.animate({ left: currentLeftValue + "px" }, 500);
+    //    }
+    //});
+});
---
+++ js11-12/js/jquery.plugin.js
@@ -0,0 +1,42 @@
+﻿(function ($) {
+    $.fn.myFirstPlugin = function (imgs) {
+
+        var $div = this;
+        var $leftButton = $('<div class="carousel-arrow-left"><span>Влево</span></div>');
+        var $rightButton = $('<div class="carousel-arrow-right"><span>Вправо</span></div>');
+
+        var $carouselContent = '<div class="carousel-hider"><ul class="carousel-list">';
+
+        for (var i = 0; i < imgs.length; i++) {
+            $carouselContent = $carouselContent + '<li class="carousel-element"><img src="' + imgs[i].src + '"><p>' + imgs[i].text + '</p></li>';
+        }
+
+        $carouselContent = $carouselContent + '</ul></div>';
+
+        $div.append($leftButton);
+        $div.append($carouselContent);
+        $div.append($rightButton);
+
+        var pixelsOffset = 125;
+        var currentLeftValue = 0;
+        var elementsCount = imgs.length;
+        var minimumOffset = -((elementsCount - 5) * pixelsOffset);
+        var maximumOffset = 0;
+
+        $leftButton.click(function () {
+            if (currentLeftValue != maximumOffset) {
+                currentLeftValue += 125;
+                $('.carousel-list').animate({ left: currentLeftValue + "px" }, 500);
+            }
+        })
+
+        $rightButton.click(function () {
+            debugger
+            if (currentLeftValue != minimumOffset) {
+                currentLeftValue -= 125;
+                $('.carousel-list').animate({ left: currentLeftValue + "px" }, 500);
+            }
+        })
+    }
+    return this;
+})(jQuery);
